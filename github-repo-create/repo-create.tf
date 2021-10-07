cfcb9092c70cae08fd05c97f64df918729732477
Jorge Arias <jorgeariasgil@gmail.com>
2021-10-07 15:25:43 +1100

    trying something

--- github-repo-create/repo-create.tf
+++ github-repo-create/repo-create.tf
@@ -5,6 +5,7 @@ resource "github_repository" "new_repo" {
   name        = var.repo_name
   description = var.repo_description
   visibility  = "public"
+  auto_init   = true
 
   delete_branch_on_merge = true
   archive_on_destroy     = true

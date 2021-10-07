b09da2ddf718a4b9b8491dea7829950395240e23
Jorge Arias <jorgeariasgil@gmail.com>
2021-10-07 15:08:57 +1100

    add variables file to main root

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "repo_name" {
+  type        = string
+  description = "Name of the new repo"
+}
+
+variable "repo_description" {
+  type = string
+  description = "Description for the new repo"
+}
